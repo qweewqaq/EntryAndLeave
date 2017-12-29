d3a0369598b79eae6319c9f5c39895d0ac3d1729
xcxz1994 <981877654@qq.com>
2017-12-29 10:50:25 +0800

    20171229

--- FormComponent/LeaveApplyForm.js
+++ FormComponent/LeaveApplyForm.js
@@ -1,8 +1,15 @@
+//员工主动辞职
 import React,{Component} from 'react';
 import {Card,DatePicker,Button,Input,Select,Row,Col,Tag} from 'antd';
 const { TextArea } = Input;
-
+import $ from 'jquery';
 export default class LeaveApplyForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            display:'none'
+        }
+    }
     render(){
         return(
             <Card title="员工辞职单" style={{ width: '80%' }}>
@@ -10,8 +17,8 @@ export default class LeaveApplyForm extends Component {
                     <Col span={6}>
                         <Input placeholder="请输入您的姓名"  style={{width:160}} id="name"/>
                     </Col>
-                    <Col span={3}> <Tag color="#108ee9" style={{height:25}}>部门</Tag></Col>
-                    <Col span={6}>
+                    <Col span={3} > <Tag color="#108ee9" style={{height:25}}>部门</Tag></Col>
+                    <Col span={6} style={{marginLeft:-25}}>
                         <Select style={{ width: 160 }} id="applyclass" onChange={value => this.handleChange4(value)}>
                             <Option value="病假">病假</Option>
                             <Option value="产假">产假</Option>
@@ -21,7 +28,7 @@ export default class LeaveApplyForm extends Component {
                         </Select>
                     </Col>
                     <Col span={3}> <Tag color="#108ee9" style={{height:25}}>职务</Tag></Col>
-                    <Col span={6}>
+                    <Col span={6} style={{marginLeft:-25}}>
                         <Select style={{ width: 160 }} id="applyclass" onChange={value => this.handleChange4(value)}>
                             <Option value="病假">病假</Option>
                             <Option value="产假">产假</Option>
@@ -31,8 +38,9 @@ export default class LeaveApplyForm extends Component {
                         </Select>
                     </Col>
                 </Row>
+                <div style={{height:20}}></div>
+                <Row >
 
-                <Row offset={2}>
                     <Col span={3}> <Tag color="#108ee9" style={{height:25}}>入职时间</Tag></Col>
                     <Col span={9} >
 
@@ -41,6 +49,41 @@ export default class LeaveApplyForm extends Component {
                     <Col span={3}> <Tag color="#108ee9" style={{height:25}}>离职时间</Tag></Col>
                     <Col span={9}>
                         <DatePicker onChange={value => this.handleChange(value)} id="date"/>
+                    </Col>
+                </Row>
+                <div style={{height:20}}></div>
+                <Row >
+                    <Col span={3}> <Tag color="#108ee9" style={{height:25}}>辞职原因</Tag></Col>
+                    <Col span={19} >
+                        <TextArea rows={8} style={{width:700,height:150}} />
+                    </Col>
+                </Row>
+                <div style={{height:20}}></div>
+                <Row style={{display:this.state.display}}>
+                    <Col span={3}> <Tag color="#108ee9" style={{height:25}}>部门主管意见</Tag></Col>
+                    <Col span={19} >
+                        <TextArea rows={8} style={{width:700,height:150}} />
+                    </Col>
+                </Row>
+                <div style={{height:20}}></div>
+                <Row >
+                    <Col span={3}> <Tag color="#108ee9" style={{height:25}}>项目部负责人意见</Tag></Col>
+                    <Col span={19} >
+                        <TextArea rows={8} style={{width:700,height:150}} />
+                    </Col>
+                </Row>
+                <div style={{height:20}}></div>
+                <Row >
+                    <Col span={3}> <Tag color="#108ee9" style={{height:25}}>综合部意见</Tag></Col>
+                    <Col span={19} >
+                        <TextArea rows={8} style={{width:700,height:150}}  />
+                    </Col>
+                </Row>
+                <div style={{height:20}}></div>
+                <Row >
+                    <Col span={3}> <Tag color="#108ee9" style={{height:25}}>总经理意见</Tag></Col>
+                    <Col span={19} >
+                        <TextArea rows={8} style={{width:700,height:150}}  />
                     </Col>
                 </Row>
             </Card>
